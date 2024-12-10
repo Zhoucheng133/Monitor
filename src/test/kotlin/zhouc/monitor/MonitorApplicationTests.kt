99fb21279e4f1377be2efbf7ece36271bc6bd234
zhoucheng <zhoucheng133@outlook.com>
2024-12-10 19:37:14 +0800

    first commit

---
+++ src/test/kotlin/zhouc/monitor/MonitorApplicationTests.kt
@@ -0,0 +1,13 @@
+package zhouc.monitor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
